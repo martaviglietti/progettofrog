8df45d5b556de14d80e4cc1650ab550026ce67ef
Gabry324 <g.stampatori@studenti.unica.it>
2025-06-18 02:28:46 +0200

    Update gestionegrafica.c

--- versione_processi/gestionegrafica.c
+++ versione_processi/gestionegrafica.c
@@ -202,6 +202,153 @@ void Gestione_grafica(WINDOW* game,int pipe1[], int pipe2[], int array_pid[],int
 }
 
 
+int spostamento_ranaCoc(int id, int coc_scelto, Coccodrillo* coccodrilli, Rana* rana, Game_struct* game_struct, int fine_manche ) {
+
+    if (coc_scelto==coccodrilli[id].id) {  //controlliamo che il coccodrillo attuale sia anche quello su cui la rana era posata prima dello spostamento
+		    if ((rana->x<=2 && coccodrilli[id].dir==-1) || (rana->x>=LARGHEZZA_GIOCO-3 && coccodrilli[id].dir==1)) {  //controllo se la rana si trova agli estremi della mappa			
+			if (RanaSuCoccodrillo(rana,coccodrilli)!=coc_scelto) {	//se la rana non viene considerata più sopra l'attuale coccodrillo allora è caduta in acqua sul bordo della mappa 
+			    game_struct->vite--;
+			    game_struct->score-=10;
+			    
+			    return 0;
+			}	
+		    } else {	 //altrimenti la rana si muove con il coccodrillo (ovvero non si trovava agli estremi della mappa)					
+		        rana->x+=coccodrilli[id].dir;
+		    }               			
+    }
+    return fine_manche;
+
+}
+
+void killa_granata(Granata* granate, int array_pid[]){
+    //controllo per killare il processo granate una volta morte
+    if (granate[0].alive==0 && granate[1].alive==0 && array_pid[IDGRANATE]!=0) { 
+	   
+	killProcess(array_pid[IDGRANATE]);	
+	array_pid[IDGRANATE]=0;
+    }
+
+}
+
+
+void collisione_GranataProiettile(int pipe2, Granata* granate, Proiettile proiettili[], int array_pid[], Game_struct* game_struct){
+
+    
+	for (int i=0; i<2; i++) {
+	    for(int j=0; j<15;j++){
+	    
+		    if (granate[i].x==proiettili[j].x && granate[i].y==proiettili[j].y && proiettili[j].alive==1 && granate[i].alive==1) {
+			killProcess(array_pid[IDPROIETTILE+j]);  //killiamo il proiettile
+			array_pid[IDPROIETTILE+j]=0;
+			//impostiamo a morti il proiettile e la granata
+			proiettili[j].alive=0;
+			granate[i].alive=0;
+			granate[i].x=-1;
+			write(pipe2,&i, sizeof(int));  //avverte il processo granata che una delle granate è morta
+			game_struct->score+=5;
+	 	    }
+	    }
+	}
+
+
+}
+
+
+void collisione_ProiettileBordi(int array_pid[], Proiettile proiettili[]){
+    for(int i=0; i<15; i++){
+    
+	    if((proiettili[i].x<1 || proiettili[i].x>LARGHEZZA_GIOCO-2) && proiettili[i].alive==1){
+		
+			killProcess(array_pid[IDPROIETTILE+i]);
+			array_pid[IDPROIETTILE+i]=0;
+			proiettili[i].alive=0;
+		}
+
+    }
+}
+
+void collisione_GranataBordi(int pipe2, Granata* granate){
+    
+    
+    for (int i=0; i<2;i++) {
+	if((granate[i].x<1 || granate[i].x>LARGHEZZA_GIOCO-2) && granate[i].alive==1 && granate[i].x!=-1) {
+	    granate[i].alive=0;
+	    granate[i].x=-1;  
+	    write(pipe2,&i, sizeof(int));  //scriviamo al processo granate quale delle due granate è morta
+	    	
+	}
+    }
+
+}
+
+
+
+
+//funzione che controlla se la rana si trova in mappa
+int RanaInFinestra(Rana* rana, Temp* temp){
+
+    if(rana->x+temp->x>78) return 0;
+    if(rana->x+temp->x<2) return 0;
+    if(rana->y+temp->y>43) return 0;
+    if(rana->y+temp->y<7) return 0;
+    return 1;
+
+}
+
+//funzione di controllo collisione rana-proiettile
+int CollisioneRanaProiettile(Rana*rana,Proiettile proiettili[], Game_struct* game_struct, int fine_manche){
+
+    for(int i=0;i<15;i++){
+    
+    
+    
+	    if((rana->x+1==proiettili[i].x || rana->x-1==proiettili[i].x) && rana->y==proiettili[i].y && proiettili[i].alive) {
+		game_struct->vite--;
+		game_struct->score-=15;
+		return 0;
+	    }
+    }
+    
+    return fine_manche;
+       
+}
+
+//funzione di controllo se la rana è su una tana
+int RanaSuTana(Rana* rana, Game_struct* game_struct){
+	 
+    for (int i=0;i<5;i++) {
+        if (rana->x>=8+(15)*i && rana->x<8+(15)*i+5 && game_struct->tane[i]==0) {
+   	    game_struct->tane[i]=1;
+ 	    return 1;
+ 	}
+    }
+    return 0;
+}
+	 	
+	 	
+	 		
+
+
+
+
+//funzione che verifica se la rana è su un coccodrillo
+int RanaSuCoccodrillo(Rana* rana, Coccodrillo *coccodrilli){
+    for (int i = 0; i < MAX_CROCODILES; i++) {
+        if (coccodrilli[i].alive && rana->y == coccodrilli[i].y &&  rana->x-1>= coccodrilli[i].x-4 && rana->x+1 <= coccodrilli[i].x+4) {
+            
+            return coccodrilli[i].id; 
+            
+        }
+    }
+    return -1; 
+}
+
+
+
+
+
+
+
 
 
 
