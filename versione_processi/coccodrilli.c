b527e28cf39cc1b3656c1c8e96915fda390a4a5f
Gabry324 <g.stampatori@studenti.unica.it>
2025-06-18 02:25:35 +0200

    Update coccodrilli.c

--- versione_processi/coccodrilli.c
+++ versione_processi/coccodrilli.c
@@ -192,3 +192,35 @@ void sparaProiettile(int id,int identificatore_coc, Coccodrillo* coccodrilli,int
        
     }
 }
+
+
+
+//funzione che assegna una velocità a ogni flusso
+void def_vel_flussi(Flusso *flussi, int vel){
+	
+    
+    int altezza_base=37;  //posizione del primo flusso (quello più in basso)
+    int dimensione_flussi=3;  //larghezza dei flussi
+    for (int i=0;i<8;i++) {
+        flussi[i].y=altezza_base - i*dimensione_flussi;
+	flussi[i].speed= rand_funz(vel - 10000,vel+10000);
+    }
+}
+
+
+
+
+//funzione che assegna una direzione ad ogni flusso
+void def_dir_flussi(Flusso *flussi){
+    for (int i=0;i<8;i++) {
+	if (i>=1) {  //tutti i flussi dal secondo in poi hanno direzione opposta al flusso precedente
+	    flussi[i].dir= -flussi[i-1].dir;
+
+	} else {
+	    //impostiamo la direzione del primo flusso in modo randomico
+	    flussi[i].dir= rand()%2;
+	    if (flussi[i].dir==0) flussi[i].dir=1;
+	    else flussi[i].dir=-1;
+	}
+    }	
+}
