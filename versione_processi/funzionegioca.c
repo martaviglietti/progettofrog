9c0a5c76575273854646c799adae4ba4043c42cc
Gabry324 <g.stampatori@studenti.unica.it>
2025-06-18 02:21:25 +0200

    Update funzionegioca.c

--- versione_processi/funzionegioca.c
+++ versione_processi/funzionegioca.c
@@ -1,68 +1,70 @@
 #include "header.h"
 
-Game_struct startGame(WINDOW *game, Stat_game stat_game) {			
+Game_struct startGame(WINDOW *game,Stat_game stat_game){			
     
-    //CREAZIONE PIPE-----------------------------------------------------------------------------------------------------
-    
-    int pipe1[2];	//quella per comunicare tra consumatore e produttore;
-    int pipe2[2];	//quella per comunicare alla granata se è morta;
+    //Creazione pipe;
+    int pipe1[2];  //per comunicare tra consumatore e produttori;
+    int pipe2[2];  //per comunicare alla granata se è morta;
     pipe(pipe1);        
-    pipe(pipe2);	
-
-
-
-    //INIZIALIZZAZIONE STATO DEL GIOCO-------------------------------------------------------------------------------------------------
-    int tane_occupate=0; //inizializzo le tane che dovranno essere occupate
-    Game_struct game_struct; // creo una variabile di tipo struttura che tiene traccia dello stato del gioco 
-    game_struct.score=0; //inizializzo il punteggio a 0 
-    game_struct.vite=stat_game.vite; //inizializzo il numero di vite co le ipostazioni iniziali 
-    game_struct.game;
-
-
-    //Imposto ogni tana come non occupata--------------------------------------------------------------------------------------------
-        for(int i=0;i<TANE -1;i++){
-    	game_struct.tane[i]=0;
+    pipe(pipe2);
+    sblocca_fd(pipe1[0]);  //sblocchiamo la pipe1	
+    sblocca_fd(pipe2[0]);  //sblocchiamo la pipe2
+   
+    //Inizializziamo variabili di gestione della partita
+    int tane_occupate=0;
+    Game_struct game_struct;
+    game_struct.score=0; 		//contiene lo score di tutto il game
+    game_struct.vite=stat_game.vite;    //contiene numero di vite rimaste
+    game_struct.tempo=stat_game.tempo;  //contiene tempo rimasto nella manche
+   
+    for (int i=0;i<TANE-1;i++) {
+        game_struct.tane[i]=0;
     }
     
-    //creo un array di flussi di tipo FLUSSO----------------------------------------------------------------
-    Flusso flussi[8];  
-    def_vel_flussi(flussi,stat_game.velocità_coccodrilli); 
-
-    //creo un array di pid per tenere traccia dei processi creati --------------------------------------------------------------------                           
-    int array_pid[N_PID];  
-    for(int i=0;i<N_PID;i++){	
-    	array_pid[i]=0;
-    }
+    Flusso flussi[8];
+    def_vel_flussi(flussi,stat_game.velocità_coccodrilli); //definisco velocità di ogni flusso                
+    int array_pid[N_PID];  //array contenente i pid dei processi creati  
     
-    //LOOP PRINCIPALE DEL GIOCO-------------------------------------------------------------------------------------------
-    while(true){
-    	game_struct.tempo=stat_game.tempo; //resetto tempo manche;
-    	def_dir_flussi(flussi);	//definisco le direzioni dei flussi
-    	creazione_processi(flussi,array_pid, pipe1, game);//creo i processi necessari per la gestione del gioco	
-    	Gestione_grafica(game,pipe1,pipe2,array_pid,stat_game.velocità_proiettili,&game_struct);
-		//aggiorno graficamente  il gico basandomi sullo stato corrente
-    	kill_processi(array_pid);		//killiamo tutto per ricominciare dopo ogni ciclo
-   	
-        //le modifiche al numero di vite e allo score sono fatte all interno della render;
-        if(game_struct.vite==0){ //sono finite le vite
- 	    	game_struct.game=0;
- 	    	return game_struct; 
-    	}
-
-        //le modifiche alle tane occupate sono fatte nella render
-   	    for(int i=0;i<5;i++){
- 	        if(game_struct.tane[i]==1){
-  	        	tane_occupate++;
-  	        }
-        }
-        if(tane_occupate==5){		//tutte le tane occupate;
-            game_struct.game==1;
-            return game_struct;
-        }
-        tane_occupate=0;   //riazzero per il prossimo conteggio;
+    int a;
+    
+    while (true) {
         
-        wclear(game);
-        wrefresh(game);     
-    }
- }
+        def_dir_flussi(flussi);  //definisce direzione di ogni flusso					
+    	creazione_processi(flussi,array_pid, pipe1,game);  //crea tutti i processi
+	Gestione_grafica(game,pipe1,pipe2,array_pid,stat_game.velocità_proiettili,&game_struct);  //funzione di gestione grafica
+    	game_struct.tempo=stat_game.tempo; 	
+	svuota_pipe(pipe1[0]);  //svuotiamo la pipe per eliminare eventuali elementi rimasti all'interno
+	
+	if(read(pipe1[0],&a,sizeof(int))!= -1){
+	    perror("roba nella pipe");
+	}
+	
+
+
+   	kill_processi(array_pid);		
+   	
+        //Controlli di fine manche
+        if (game_struct.vite==0) { 
+ 	    game_struct.game=0;
+ 	    game_struct.score-=50;
+ 	    return game_struct;  //uscita per vite finite
+	}
+	
+   	for (int i=0;i<5;i++) {
+ 	    if(game_struct.tane[i]==1){
+  	        tane_occupate++;
+  	    }
+ 	}
+ 	
+ 	if (tane_occupate==5) {		
+ 	    game_struct.game=1;
+ 	    game_struct.score+=100;
+	    return game_struct;  //uscita per tane chiuse
+ 	}
+	tane_occupate=0;
+   
+	wclear(game);
+	wrefresh(game);     
+	}
+}
 
