a62ab415331ce5df77e2f10f20b2fe6f1b878e97
Gabry324 <g.stampatori@studenti.unica.it>
2025-06-18 02:34:21 +0200

    Update header.h

--- versione_processi/header.h
+++ versione_processi/header.h
@@ -1,19 +1,8 @@
-#include <ncurses.h>
-#include <stdbool.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <ncurses.h>
-#include <sys/wait.h>
-#include <sys/time.h>
-#include "menu.h"
-#include <string.h>
-#include <fcntl.h>
-#define MAX_CROCODILES 8
+#define MAX_CROCODILES 24
+#define LARGHEZZA_GIOCO 81
+#define ALTEZZA_GIOCO 49
 #define POS_SPAWN_COC_SINISTRA -4
-#define POS_SPAWN_COC_DESTRA 83
+#define POS_SPAWN_COC_DESTRA 84
 #define ALTEZZARANA 2
 #define LARGHEZZARANA 3
 #define ALTEZZACOCCODRILLO 2
@@ -25,22 +14,18 @@
 #define SPONDA_SUPERIORE 5
 #define N_PID 100
 //definizione id da usare;
-#define IDTIME 40
+#define IDTIME 45
 #define IDCOC 0
-#define IDRANA 20
-#define IDGRANATE 30
-#define IDPROIETTILE 50
-#define IDAGGIUNTAPID 60
+#define IDRANA 50
+#define IDGRANATE 60
+#define IDPROIETTILE 70
+#define IDAGGIUNTAPID 30
 #define IDMORTE -10
-#define IDRICHIESTA 70
+#define IDRICHIESTA 90
 
-#ifndef HEADER_H
-#define HEADER_H
 
-// Opzioni del menu
-static const char *OPZIONI[] = {
-    "Inizia gioco", "Crediti", "Esci"
-};
+
+
 
 
 
@@ -76,9 +61,10 @@ typedef struct{
     int x;
     int dir;
     int alive;
+    int wait;
 }Coccodrillo;
 
-typedef struct{double elapsed_time;
+typedef struct{
     int id;
     int y;
     int x;
@@ -106,6 +92,63 @@ typedef struct{
 	int y;
 	int info;
 }Temp;
+
+
+int gameWin(WINDOW *game, int score);
+int gameOver(WINDOW *game, int score);
+int scegliDifficolta(WINDOW *game);
+void windowGeneration(WINDOW *game, int maxX, int maxY, Game_struct* game_struct);
+Game_struct startGame(WINDOW *game,Stat_game stat_game);
+void Gestione_grafica(WINDOW* game,int pipe1[], int pipe2[], int array_pid[],int vel_proiettili, Game_struct* game_struct);
+int CollisioneRanaProiettile(Rana* rana,Proiettile proiettile[], Game_struct* game_struct, int fine_manche);
+int RanaSuTana(Rana*rana, Game_struct* game_struct);
+int RanaSuCoccodrillo(Rana *rana, Coccodrillo *coccodrilli);
+void creazione_processi(Flusso *flussi, int array_pid[N_PID], int pipe1[],WINDOW* game);
+void funzione_gestione_coccodrilli(Flusso *flussi,int pipe1[]);
+void funzione_coccodrillo(Temp coccodrillo,Flusso flussi[8],int id_flusso_scelto, int pipe1[]);
+void funz_proiettile(int id,Coccodrillo coccodrillo,int vel_proiettile, int pipe1[]);
+void tempo(int pipe1[]);
+void kill_processi(pid_t* pid);
+void def_vel_flussi(Flusso *flussi, int velocità_coccodrillo);
+void def_dir_flussi(Flusso *flussi);
+int menu(WINDOW *game, const char *title, const char *options[], int num_options);
+void credits(WINDOW *game);
+int rand_funz(int min, int max);
+void frog(WINDOW* game,int pipe1[2]);
+void sparaGranata(int startX, int startY,int vel_proiettile ,int pid_array[],int pipe1[], int pipe2[]);
+void sparaProiettile(int id, int identificatore_coc,  Coccodrillo* coccodrilli,int vel_proiettile,int pid_array[], int pipe1[]);
+void granata(int startX, int startY,int vel_proiettile,int pipe1[], int pipe2[]);
+void creazione_colori();
+void draw_proiettile(WINDOW* game, Proiettile proiettile[]);
+void draw_granate(WINDOW* game, Granata granate[2]);
+void draw_frog(WINDOW *game, Rana* rana);
+void drawCoccodrilli(WINDOW *game, Coccodrillo *coccodrilli);
+void sblocca_fd(int fd);
+void killProcess(pid_t pid);
+int RanaInFinestra(Rana* rana, Temp*temp);
+void blocca_fd(int fd);
+void svuota_pipe(int pipe_fd);
+void print_tempo(WINDOW* game,Game_struct* game_struct, int tempo);    
+void punteggio_tempo(Game_struct* game_struct);         
+void collisione_GranataBordi(int pipe, Granata* granate);
+void collisione_ProiettileBordi(int array_pid[], Proiettile proiettile[]);
+void collisione_GranataProiettile(int pipe2, Granata* granate, Proiettile proiettile[], int array_pid[], Game_struct* game_struct);
+void killa_granata(Granata* granate, int array_pid[]);
+void riattivare_coccodrilli(Coccodrillo* coccodrilli, int distanze_coc[], int array_pid[]);
+void attesa_coccodrilli(int id, Coccodrillo* coccodrilli, int distanze_coc[], int array_pid[]);
+void controllo_stato_coccodrillo(int id,Coccodrillo* coccodrilli);
+int spostamento_ranaCoc(int id, int coc_scelto, Coccodrillo* coccodrilli, Rana* rana, Game_struct* game_struct , int fine_manche);
+void impostazioni_gioco(Stat_game* stat_game, int difficoltà );
+#ifndef HEADER_H
+#define HEADER_H
+
+// Opzioni del menu
+static const char *OPZIONI[] = {
+    "Inizia gioco", "Crediti", "Esci"
+};
+
+
+
 
 // Dichiarazioni delle variabili globali
 extern const char *frog_sprite[2];
