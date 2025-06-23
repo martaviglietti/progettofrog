ea4ddf15d7fe1a903e84fa40f5732c24dafd8020
Gabry324 <g.stampatori@studenti.unica.it>
2025-06-23 07:46:59 +0200

    Update rana.c

--- versione_processi/rana.c
+++ versione_processi/rana.c
@@ -1,17 +1,17 @@
 #include "header.h"
-void frog(WINDOW* game,int pipe1[2]){
+void funzione_rana(WINDOW* finestra_gioco,int pipe1[2]){
     close(pipe1[0]);
     
     int key;
-    Temp rana={IDRANA,0,0,0};  //messaggio usato per inviare alla funzione di controllo i movimenti della rana     
-    Temp granata={IDGRANATE+2,0,0,0};  //messaggio con id apposito per la creazione delle granate
-    keypad(game, true);
+    Temp rana={ID_RANA,0,0,0};  //messaggio usato per inviare alla funzione di controllo i movimenti della rana     
+    Temp granata={ID_GRANATE+2,0,0,0};  //messaggio con id apposito per la creazione delle granate
+    keypad(finestra_gioco, true);
     
 
     while (true) {
     
-        //Cattura l'input da tastiera
-        key = (int)wgetch(game);
+        //cattura l'input da tastiera
+        key = (int)wgetch(finestra_gioco);
         rana.x=0;
         rana.y=0;
 
@@ -46,40 +46,47 @@ void frog(WINDOW* game,int pipe1[2]){
     }
 }
 
+
 //funzione che gestisce la creazione del processo granata
-void sparaGranata(int startX, int startY,int velocità_proiettile ,int pid_array[],int pipe1[], int pipe2[]){
+void spara_granata(int inizioX, int inizioY,int velocità_proiettile ,int pid_array[],int pipe1[], int pipe2[]){
 
-    pid_array[IDGRANATE]= fork(); //Crea un nuovo processo per la granata
+    pid_array[ID_GRANATE]= fork(); //Crea un nuovo processo per la granata
 
-    if (pid_array[IDGRANATE] < 0) {
-        perror("Errore nella creazione del processo granata");
-        exit(0); 
+    if (pid_array[ID_GRANATE] < 0) {
+        perror("Errore nella creazione del processo granata: ");
+        exit(1); 
     }
 
-    if (pid_array[IDGRANATE] == 0) {  
+    if (pid_array[ID_GRANATE] == 0) {  
         
-        granata(startX, startY,velocità_proiettile, pipe1,pipe2);  //chiamiamo la funzione granata all'interno del processo figlio
+        funzione_granata(inizioX, inizioY,velocità_proiettile, pipe1,pipe2);  //chiamiamo la funzione granata all'interno del processo figlio
     }
 }
 
+
+
+
+
+
 //funzione che gestisce il processo granata
-void granata(int startX, int startY, int velocità_proiettile, int pipe1[], int pipe2[]){		
+void funzione_granata(int inizioX, int inizioY, int velocità_proiettile, int pipe1[], int pipe2[]){		
     						
     close(pipe2[1]);
     close(pipe1[0]);
     Temp granate[2];
-    int messaggio;
+    int messaggio;  //variabile per leggere il messaggio dal gestore grafico
+    
     //variabili che gestiscono lo stato delle granate
     int alive_destra=1;
     int alive_sinistra=1;
    
     //impostazione iniziale delle granate (id e posizione di partenza)
-    granate[0].id=IDGRANATE; 
-    granate[1].id=IDGRANATE + 1;
-    granate[0].x=startX - 2;
-    granate[1].x=startX + 2; 			
-    granate[0].y=startY;
-    granate[1].y=startY;   
+    granate[0].id=ID_GRANATE; 
+    granate[1].id=ID_GRANATE + 1;
+    granate[0].x=inizioX - 2;
+    granate[1].x=inizioX + 2; 			
+    granate[0].y=inizioY;
+    granate[1].y=inizioY;   
     granate[0].info=0;
     granate[1].info=0;
     
@@ -89,8 +96,8 @@ void granata(int startX, int startY, int velocità_proiettile, int pipe1[], int 
     while (true) {
     	
     	
-    	while(read(pipe2[0],&messaggio,sizeof(int))!=-1){  //leggiamo se ci sono messaggi dalla funzioni di controllo per granate morte
-            if (messaggio){
+    	while (read(pipe2[0],&messaggio,sizeof(int))!=-1) {  //leggiamo se ci sono messaggi dal gestore grafico per eventuali granate morte
+            if (messaggio) {
                 
                 alive_destra=0;
             } else { 
@@ -98,6 +105,8 @@ void granata(int startX, int startY, int velocità_proiettile, int pipe1[], int 
                 	
             }
         }
+        
+        //per ogni granata scriviamo nella pipe le nuove coordinate solo se ancora vive
         if (alive_destra) {
 	    write(pipe1[1],&granate[1],sizeof(Temp));	 
 	    granate[1].x++; 
