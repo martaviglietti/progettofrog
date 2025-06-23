c43e291a8dc34852578138b190fa8bf1d0fb56ed
Gabry324 <g.stampatori@studenti.unica.it>
2025-06-23 07:50:52 +0200

    Update menu.c

--- versione_processi/menu.c
+++ versione_processi/menu.c
@@ -5,72 +5,75 @@ static const char *OPZIONI[] = {
     "Inizia gioco", "Crediti", "Esci"
 };
 
-int menu(WINDOW *game, const char *title, const char *options[], int num_options){
+int menu(WINDOW *finestra_gioco, const char *title, const char *options[], int num_options){
     flushinp();  //elimina input residuo
-    keypad(game, true);  //abilita l'input da tastiera
+    keypad(finestra_gioco, true);  //abilita l'input da tastiera
 
-    int position = 0;
-    int gameLINES = getmaxy(game);
-    int gameCOLS = getmaxx(game);
+    int posizione = 0;
+    int gameLINES = getmaxy(finestra_gioco);
+    int gameCOLS = getmaxx(finestra_gioco);
+    int scelta;
 
     while (true) {
-        wclear(game);
-        box(game, ACS_VLINE, ACS_HLINE);
+        wclear(finestra_gioco);
+        box(finestra_gioco, ACS_VLINE, ACS_HLINE);
 
-        mvwprintw(game, gameLINES / 2 - 5, gameCOLS / 2 - strlen(title)/2, "%s", title);
+        mvwprintw(finestra_gioco, gameLINES / 2 - 5, gameCOLS / 2 - strlen(title)/2, "%s", title);
+        
+        //mostriamo le opzioni
         for (int i = 0; i < num_options; i++) {
-            if (position == i) {
-                wattron(game, COLOR_PAIR(9));
+            if (posizione == i) {
+                wattron(finestra_gioco, COLOR_PAIR(9));
             }
 
             int x = gameCOLS / 2 - (strlen(options[i]) / 2);
-            mvwprintw(game, gameLINES / 2 - 1 + i, x, "%s", options[i]);
+            mvwprintw(finestra_gioco, gameLINES / 2 - 1 + i, x, "%s", options[i]);
 
             
-            wattroff(game, COLOR_PAIR(9));
+            wattroff(finestra_gioco, COLOR_PAIR(9));
         }
-
-        int choice = wgetch(game);
-        switch (choice) {
+        
+        //gestiamo l'eventuale movimento del cursore sulle opzioni
+        scelta = wgetch(finestra_gioco);
+        switch (scelta) {
             case KEY_DOWN:
-                position++;
-                if (position >= num_options) position = 0;
+                posizione++;
+                if (posizione >= num_options) posizione = 0;
                 break;
             case KEY_UP:
-                position--;
-                if (position < 0) position = num_options-1;
+                posizione--;
+                if (posizione < 0) posizione = num_options-1;
                 break;
             case 10: // Invio
-                return position;
+                return posizione;
         }
 
-        wrefresh(game);
+        wrefresh(finestra_gioco);
     }
 }
 
 
 //funzione che mostra i crediti
-void credits(WINDOW *game){
-    wclear(game);
-    box(game, ACS_VLINE, ACS_HLINE);
+void crediti(WINDOW *finestra_gioco){
+    wclear(finestra_gioco);
+    box(finestra_gioco, ACS_VLINE, ACS_HLINE);
 
-    int gameLINES = getmaxy(game);
-    int gameCOLS = getmaxx(game);
+    int gameLINES = getmaxy(finestra_gioco);
+    int gameCOLS = getmaxx(finestra_gioco);
 
-    mvwprintw(game, gameLINES / 2 - 2, gameCOLS / 2 - 15, "Marta Viglietti");
-    mvwprintw(game, gameLINES / 2 - 1, gameCOLS / 2 - 15, "Gabriele Stampatori");
-    mvwprintw(game, gameLINES / 2 + 1, gameCOLS / 2 - 17, "Premi un tasto per tornare al menu...");
+    mvwprintw(finestra_gioco, gameLINES / 2 - 2, gameCOLS / 2 - 15, "Marta Viglietti");
+    mvwprintw(finestra_gioco, gameLINES / 2 - 1, gameCOLS / 2 - 15, "Gabriele Stampatori");
+    mvwprintw(finestra_gioco, gameLINES / 2 + 1, gameCOLS / 2 - 17, "Premi un tasto per tornare al menu...");
 
-    wrefresh(game); 
-    wgetch(game);
+    wrefresh(finestra_gioco); 
+    wgetch(finestra_gioco);
 }
 
 
-//funzione utile per creare un numero random tra un minimo e un massimo (compresi)
-int rand_funz(int min, int max){
+//funzione utile per creare un numero numero_random tra un minimo e un massimo (compresi)
+int numero_random(int min, int max){
 
     return min + rand() % (max-min+1);
 
 }
-
 
